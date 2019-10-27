eb627011276077aa52a027f96d057854cbace4df
Guy Smoilov <611655+guysmoilov@users.noreply.github.com>
2019-10-27 16:36:39 +0200

    Change pip package name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dovpanda",
+    name="dovpanda-by-guy",
     version="0.0.1",
     author="Dean Langsam",
     author_email="deanla@gmail.com",
     description="Directions overlay for working with pandas in an analysis environment",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/DeanLa/dovpanda",
+    url="https://github.com/guysmoilov/dovpanda",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
