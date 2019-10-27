588ec20575a877d25e5fbcd462d930aad3213450
Guy Smoilov <611655+guysmoilov@users.noreply.github.com>
2019-10-27 16:49:10 +0200

    Actually update the package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dovpanda-by-guy",
-    version="0.0.1",
+    version="0.0.3",
     author="Dean Langsam",
     author_email="deanla@gmail.com",
     description="Directions overlay for working with pandas in an analysis environment",
